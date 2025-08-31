0bd806b4d6a47ee95161fcea028f471c101108e7
codyempress <codyempress@gmail.com>
2025-08-31 20:34:54 +0300

    Register model in admin, customize aspects

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from bookshelf.models import Book
+from .models import Book
 
 admin.site.register(Book)
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
 
-    list_display = ("author", "title", "publication_year") #columns to display in teh Admin list view
+    list_display = ("title", "author", "publication_year") #columns to display in teh Admin list view
     list_filter = ("Author") #Filters on the right hand side
+    search_fields = ("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
